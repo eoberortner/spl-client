2187030c0ecef3e2905eb13ddcc8e168f53c39c0
eoberortner <eoberortner@gmail.com>
2016-05-22 20:50:01 -0700

    documentation + check if username/password is specified

--- src/test/java/gov/doe/jgi/spl/client/DemoClient.java
+++ src/test/java/gov/doe/jgi/spl/client/DemoClient.java
@@ -5,24 +5,56 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
 
+
+/**
+ * The DemoClient demonstrates the use of the 
+ * SPL client's functions.
+ * 
+ * @author Ernst Oberortner
+ *
+ */
 public class DemoClient {
 
-	public static Properties loadUsernameAndPassword() 
+	/**
+	 * The loadUsernameAndPassword() method loads the username 
+	 * and password from a local .properties file
+	 * 
+	 * @param filename ... the name of the .properties file
+	 * 
+	 * @return a Properties object containing all properties that 
+	 * are specified in the .properties file
+	 * 
+	 * @throws Exception ... if something went wrong while loading 
+	 * the properties, such as the file does not exist or the username 
+	 * and password keys don't exist
+	 */
+	public static Properties loadUsernameAndPassword(final String filename) 
 			throws Exception {
 		
+		// load the properties
 		Properties prop = new Properties();
-
 		try (
 			InputStream in = 
-				Files.newInputStream(Paths.get("spl.properties"));
+				Files.newInputStream(Paths.get(filename));
 			) {
 			
 			prop.load(in);
 		}
 		
+		// check that username and password exist
+		if(!prop.containsKey("username")) {
+			throw new Exception("No username specified!");
+		}
+		if(!prop.containsKey("password")) {
+			throw new Exception("No password specified!");
+		}
+		
 		return prop;
 	}
 	
+	/*-------------
+	 * MAIN
+	 *-------------*/
 	public static void main(String[] args) 
 			throws Exception {
 		
@@ -32,7 +64,7 @@ public class DemoClient {
 		/*
 		 * login
 		 */
-		Properties prop = loadUsernameAndPassword();
+		Properties prop = loadUsernameAndPassword("spl.properties");
 
 		client.login(
 				prop.getProperty("username").trim(), 
