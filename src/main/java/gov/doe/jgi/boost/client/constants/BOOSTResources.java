4cd82cbd0ed77366b295ac631b2e20d22c9c4d13
eoberortner <eoberortner@lbl.gov>
2016-08-10 15:32:28 -0700

    the codonJuggle method returns a String now

--- src/main/java/gov/doe/jgi/boost/client/constants/BOOSTResources.java
+++ src/main/java/gov/doe/jgi/boost/client/constants/BOOSTResources.java
@@ -2,8 +2,8 @@ package gov.doe.jgi.boost.client.constants;
 
 public class BOOSTResources {
 
-	public static final String BOOST_REST_URL = "http://localhost:8080/boost-web/rest";
-//	public static final String BOOST_REST_URL = "https://boost.jgi.doe.gov/rest/";
+//	public static final String BOOST_REST_URL = "http://localhost:8080/boost-web/rest";
+	public static final String BOOST_REST_URL = "https://boost.jgi.doe.gov/rest/";
 	
 	public static final String LOGIN_RESOURCE = "auth/login";
 	public static final String REVERSE_TRANSLATE_RESOURCE = "juggler/juggle";
