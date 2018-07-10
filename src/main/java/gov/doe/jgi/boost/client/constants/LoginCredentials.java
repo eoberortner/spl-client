b203510b452f69dd2f1080d6f998eb2160fbc36f
eoberortner <eoberortner@gmail.com>
2018-07-09 19:04:40 -0700

    released version 1.0.0-SNAPSHOT

--- src/main/java/gov/doe/jgi/boost/client/constants/LoginCredentials.java
+++ src/main/java/gov/doe/jgi/boost/client/constants/LoginCredentials.java
@@ -5,10 +5,11 @@ package gov.doe.jgi.boost.client.constants;
  * @author Ernst Oberortner
  */
 public class LoginCredentials {
-    // --alternative 1: provide your BOOST-JWT
-	public static final String mJWT = "";
 	
-	// --alternative 2: provide your user name and password
+    // -- alternative 1: provide your BOOST-JWT
+	public static final String mJWT = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJkZWZhdWx0IiwiaWF0IjoxNTMxMTcwNDM4LCJzdWIiOiJib29zdCIsImlzcyI6ImJvb3N0In0.JdRmgGpeEtzeMZk7jaCGgzgEnQax5JF3JWJE5lElIRg";
+	
+	// -- alternative 2: provide your user name and password
 	public static final String mUserName = "put your user name here";
 	public static final String mPassword = "put your password here";
 }
