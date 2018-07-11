1e63c5a426c15277f2b018dd3ef9bd1eef0d06c2
eoberortner <eoberortner@lbl.gov>
2018-07-10 17:44:36 -0700

    test

--- src/main/java/gov/doe/jgi/boost/client/constants/LoginCredentials.java
+++ src/main/java/gov/doe/jgi/boost/client/constants/LoginCredentials.java
@@ -7,7 +7,7 @@ package gov.doe.jgi.boost.client.constants;
 public class LoginCredentials {
 	
     // -- alternative 1: provide your BOOST-JWT
-	public static final String mJWT = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJkZWZhdWx0IiwiaWF0IjoxNTMxMjUyMzMwLCJzdWIiOiJib29zdCIsImlzcyI6ImJvb3N0In0.FGWEP-EU1wigd0bIFdGHem5kZ3VcIdDMiumVG_XqMWI";
+	public static final String mJWT = "";
 	
 	// -- alternative 2: provide your user name and password
 	public static final String mUserName = "put your user name here";
