0988366f5c3cfc2d6de1ab2ea3db653a8b851529
eoberortner <eoberortner@lbl.gov>
2018-08-06 09:42:02 -0700

    bug - empty was derivedFroms

--- src/main/java/gov/doe/jgi/boost/client/constants/LoginCredentials.java
+++ src/main/java/gov/doe/jgi/boost/client/constants/LoginCredentials.java
@@ -7,7 +7,7 @@ package gov.doe.jgi.boost.client.constants;
 public class LoginCredentials {
 	
     // -- alternative 1: provide your BOOST-JWT
-	public static final String mJWT = "";
+	public static final String mJWT = "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlb2Jlcm9ydG5lciIsImlhdCI6MTUzMzA4MDgyNywic3ViIjoiYm9vc3QiLCJpc3MiOiJib29zdCJ9.VXtPxChNMfHANvWtd9qGuT9F6ArYuuMfIM4aEUljGxU";
 
 	// -- alternative 2: provide your user name and password
 	public static final String mUserName = "put your user name here";
