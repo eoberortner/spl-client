bbc8633e946e757ac9daeeba93802078be6df08f
Prem0707 <premprakash0796@gmail.com>
2018-08-14 17:13:51 +0530

    code refactoring in JSON parsing of dna verification and polishing

--- src/main/java/gov/doe/jgi/boost/resopnseparser/CDSPolisingResponseParser.java
+++ src/main/java/gov/doe/jgi/boost/resopnseparser/CDSPolisingResponseParser.java
@@ -8,29 +8,41 @@ import org.json.JSONObject;
 public class CDSPolisingResponseParser {
 	
 	private JSONObject responseObject = null;
+	private String PROVENANCE = "provenance";
+	private String INPUT_DOCUMENT = "input-document";
+	private String OUTPUT_DOCUMENT = "output-document";
+	private String RESPONSE = "response";
+	private String SEQUENCE_VIOLATIONS = "sequence-violations";
+	private String VIOLATION_BEFORE = "violations-before";
+	private String VIOLATION_AFTER = "violations-after";
+	private String REPEAT = "Repeat";
+	private String SEQUENCE_PATTERNS = "Sequence Pattern";
+	private String GC_CONTENT = "Local %GC";
+	private String STATISTICS = "statistics";
+
 	
 	public CDSPolisingResponseParser(JSONObject jsonObject) {
 		this.responseObject = jsonObject;
 	}
 	
 	public String inputSequence() {
-		JSONObject object = responseObject.getJSONObject("provenance");
-		String inputSequence = object.getString("input-document");
+		JSONObject object = responseObject.getJSONObject(PROVENANCE);
+		String inputSequence = object.getString(INPUT_DOCUMENT);
 		return inputSequence;
 	}
 	
 	public String outputSequence() {
-		JSONObject object = responseObject.getJSONObject("provenance");
-		String outputSequence = object.getString("output-document");
+		JSONObject object = responseObject.getJSONObject(PROVENANCE);
+		String outputSequence = object.getString(OUTPUT_DOCUMENT);
 		return outputSequence;
 	}
 	
 	public JSONObject vioBeforePolish() {
-		JSONArray responseArray = responseObject.getJSONArray("response");
+		JSONArray responseArray = responseObject.getJSONArray(RESPONSE);
 		JSONObject vioBeforeObj = responseArray.getJSONObject(0);
-		JSONArray vioBeforeArray = vioBeforeObj.getJSONArray("violations-before");
+		JSONArray vioBeforeArray = vioBeforeObj.getJSONArray(VIOLATION_BEFORE);
 		JSONObject obj = vioBeforeArray.getJSONObject(0);
-		JSONObject seqVioObject = obj.getJSONObject("sequence-violations");
+		JSONObject seqVioObject = obj.getJSONObject(SEQUENCE_VIOLATIONS);
 		
 		return seqVioObject;
 	}
@@ -38,10 +50,10 @@ public class CDSPolisingResponseParser {
 	public ArrayList<String> repeatsBeforePolish(){
 		ArrayList<String> repeatsArrayList = new ArrayList<String>();
 		JSONObject sequenceViolationObj = vioBeforePolish();
-		JSONArray repeats = sequenceViolationObj.getJSONArray("Repeat");
+		JSONArray repeats = sequenceViolationObj.getJSONArray(REPEAT);
 		
 		for(int i=0; i< repeats.length(); i++) {
-			repeatsArrayList.add(i, repeats.getString(i));
+			repeatsArrayList.add(i, repeats.getJSONArray(i).getString(0));
 		}
 		return repeatsArrayList;
 	}
@@ -49,10 +61,10 @@ public class CDSPolisingResponseParser {
 	public ArrayList<String> seqPattBeforeVio(){
 		ArrayList<String> seqPatternsArrayList = new ArrayList<String>();
 		JSONObject sequenceViolationObj = vioBeforePolish();
-		JSONArray seqPattBeforeViolation = sequenceViolationObj.getJSONArray("Sequence Pattern");
+		JSONArray seqPattBeforeViolation = sequenceViolationObj.getJSONArray(SEQUENCE_PATTERNS);
 		
 		for(int i=0; i< seqPattBeforeViolation.length(); i++) {
-			seqPatternsArrayList.add(i, seqPattBeforeViolation.getString(0));
+			seqPatternsArrayList.add(i, seqPattBeforeViolation.getJSONArray(i).getString(0));
 		}	
 		return seqPatternsArrayList;
 	}
@@ -60,19 +72,19 @@ public class CDSPolisingResponseParser {
 	public ArrayList<String> gcContentBeforeVio(){
 		ArrayList<String> gcContentArrayList = new ArrayList<String>();
 		JSONObject sequenceViolationObj = vioBeforePolish();
-		JSONArray gcBeforeViolation = sequenceViolationObj.getJSONArray("Local %GC");
+		JSONArray gcBeforeViolation = sequenceViolationObj.getJSONArray(GC_CONTENT);
 		
 		for(int i=0; i< gcBeforeViolation.length(); i++) {
-			gcContentArrayList.add(i, gcBeforeViolation.getString(i));
+			gcContentArrayList.add(i, gcBeforeViolation.getJSONArray(i).getString(0));
 		}
 		return gcContentArrayList;
 	}
 	
 	
 	public JSONObject vioAfterPolish() {
-		JSONArray vioAfterPolishArray = responseObject.getJSONArray("violations-after");
+		JSONArray vioAfterPolishArray = responseObject.getJSONArray(VIOLATION_AFTER);
 		JSONObject vioAfterObj = vioAfterPolishArray.getJSONObject(0);
-		JSONObject seqVioObject = vioAfterObj.getJSONObject("sequence-violations");
+		JSONObject seqVioObject = vioAfterObj.getJSONObject(SEQUENCE_VIOLATIONS);
 		
 		return seqVioObject;
 	}
@@ -80,10 +92,10 @@ public class CDSPolisingResponseParser {
 	public ArrayList<String> repeatsAfterPolish(){
 		ArrayList<String> repeatsArrayList = new ArrayList<String>();
 		JSONObject sequenceViolationObj = vioAfterPolish();
-		JSONArray repeats = sequenceViolationObj.getJSONArray("Repeat");
+		JSONArray repeats = sequenceViolationObj.getJSONArray(REPEAT);
 		
 		for(int i=0; i< repeats.length(); i++) {
-			repeatsArrayList.add(i, repeats.getString(i));
+			repeatsArrayList.add(i, repeats.getJSONArray(i).getString(0));
 		}
 		return repeatsArrayList;
 	}
@@ -91,10 +103,10 @@ public class CDSPolisingResponseParser {
 	public ArrayList<String> seqPattAfterVio(){
 		ArrayList<String> seqPatternsArrayList = new ArrayList<String>();
 		JSONObject sequenceViolationObj = vioAfterPolish();
-		JSONArray seqPattAfterViolation = sequenceViolationObj.getJSONArray("Sequence Pattern");
+		JSONArray seqPattAfterViolation = sequenceViolationObj.getJSONArray(SEQUENCE_PATTERNS);
 		
 		for(int i=0; i< seqPattAfterViolation.length(); i++) {
-			seqPatternsArrayList.add(i, seqPattAfterViolation.getString(0));
+			seqPatternsArrayList.add(i, seqPattAfterViolation.getJSONArray(i).getString(0));
 		}	
 		return seqPatternsArrayList;
 	}
@@ -102,10 +114,10 @@ public class CDSPolisingResponseParser {
 	public ArrayList<String> gcContentAfterVio(){
 		ArrayList<String> gcContentArrayList = new ArrayList<String>();
 		JSONObject sequenceViolationObj = vioBeforePolish();
-		JSONArray gcAfterViolation = sequenceViolationObj.getJSONArray("Local %GC");
+		JSONArray gcAfterViolation = sequenceViolationObj.getJSONArray(GC_CONTENT);
 		
 		for(int i=0; i< gcAfterViolation.length(); i++) {
-			gcContentArrayList.add(i, gcAfterViolation.getString(i));
+			gcContentArrayList.add(i, gcAfterViolation.getJSONArray(i).getString(0));
 		}
 		return gcContentArrayList;
 	}
--- src/main/java/gov/doe/jgi/boost/resopnseparser/DNAVerificationResponseParser.java
+++ src/main/java/gov/doe/jgi/boost/resopnseparser/DNAVerificationResponseParser.java
@@ -1,10 +1,24 @@
 package gov.doe.jgi.boost.resopnseparser;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class DNAVerificationResponseParser {
+	
+	private String PROVENANCE = "provenance";
+	private String INPUT_DOCUMENT = "input-document";
+	private String OUTPUT_DOCUMENT = "output-document";
+	private String RESPONSE = "response";
+	private String VIOLATIONS = "violations";
+	private String SEQUENCE_VIOLATIONS = "sequence-violations";
+	private String REPEAT = "Repeat";
+	private String SEQUENCE_PATTERNS = "Sequence Pattern";
+	private String GC_CONTENT = "Local %GC";
+	private String MEASUREMENT_NAME = "measurement-name";
+	private String MEASUREMENT_VALUE = "measurement-value";
+	private String STATISTICS = "statistics";
 
 	private JSONObject responseObject = null;
 
@@ -13,28 +27,62 @@ public class DNAVerificationResponseParser {
 	}
 
 	public String inputSequence() {
-		JSONObject object = responseObject.getJSONObject("provenance");
-		String inputSequence = object.getString("input-document");
+		JSONObject object = responseObject.getJSONObject(PROVENANCE);
+		String inputSequence = object.getString(INPUT_DOCUMENT);
 		return inputSequence;
 	}
 	
 	public String outputSequence() {
-		JSONObject object = responseObject.getJSONObject("provenance");
-		String outputSequence = object.getString("output-document");
+		JSONObject object = responseObject.getJSONObject(PROVENANCE);
+		String outputSequence = object.getString(OUTPUT_DOCUMENT);
 		return outputSequence;
 	}
 
 	public JSONObject constraintsViolations() {
-		JSONArray responseArray = responseObject.getJSONArray("response");
-		JSONArray violationArray = responseArray.getJSONObject(0).getJSONArray("violations");
+		JSONArray responseArray = responseObject.getJSONArray(RESPONSE);
+		JSONArray violationArray = responseArray.getJSONObject(0).getJSONArray(VIOLATIONS);
 
 		JSONObject violationObject = null;
-		if (violationArray.length() >= 0 && violationObject.has("sequence-violations")) {
-			violationObject = violationArray.getJSONObject(0).getJSONObject("sequence-violations");
+		if (violationArray.length() >= 0 && violationObject.has(SEQUENCE_VIOLATIONS)) {
+			violationObject = violationArray.getJSONObject(0).getJSONObject(SEQUENCE_VIOLATIONS);
 		} else {
 			violationObject = null;
 		}
 		return violationObject;
+	}
+	
+	public ArrayList<String> repeats(){
+		ArrayList<String> repeats = new ArrayList<String>();
+		JSONObject violationObject = constraintsViolations();
+		JSONArray repeatArray = violationObject.getJSONArray(REPEAT);
+		
+		for(int i=0; i< repeatArray.length(); i++) {
+			String element = repeatArray.getJSONArray(i).getString(0);
+			repeats.add(i, element);
+		}
+		return repeats;
+	}
+	
+	public ArrayList<String> seqPatterns(){
+		ArrayList<String> seqPatternsArrayList = new ArrayList<String>();
+		JSONObject sequenceViolationObj = constraintsViolations();
+		JSONArray seqPattAfterViolation = sequenceViolationObj.getJSONArray(SEQUENCE_PATTERNS);
+		
+		for(int i=0; i< seqPattAfterViolation.length(); i++) {
+			seqPatternsArrayList.add(i, seqPattAfterViolation.getJSONArray(i).getString(0));
+		}	
+		return seqPatternsArrayList;
+	}
+	
+	public ArrayList<String> gcContent(){
+		ArrayList<String> gcContentArrayList = new ArrayList<String>();
+		JSONObject sequenceViolationObj = constraintsViolations();
+		JSONArray gcViolation = sequenceViolationObj.getJSONArray(GC_CONTENT);
+		
+		for(int i=0; i< gcViolation.length(); i++) {
+			gcContentArrayList.add(i, gcViolation.getJSONArray(i).getString(0));
+		}
+		return gcContentArrayList;
 	}
 
 	public HashMap<String, String> verificationStatistics() {
@@ -49,8 +97,8 @@ public class DNAVerificationResponseParser {
 		if (null != staticsArray) {
 			for (int index = 0; index < staticsArray.length(); index++) {
 				JSONObject staticsObject = staticsArray.getJSONObject(index);
-				String measurementName = staticsObject.getString("measurement-name");
-				String measurementValue = staticsObject.getString("measurement-value");
+				String measurementName = staticsObject.getString(MEASUREMENT_NAME);
+				String measurementValue = staticsObject.getString(MEASUREMENT_VALUE);
 				veificationStatists.put(measurementName, measurementValue);
 			}
 		}
